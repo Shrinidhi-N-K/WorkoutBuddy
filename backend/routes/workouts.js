1c91ac39cb4f2470c36c6c5f741dff1845a15a7a
shrinidhi <snkulkarni2003@gmail.com>
2024-10-07 13:20:28 +0530

    added authentication to routes and access to the authorized user only

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -6,8 +6,12 @@ const {
   deleteWorkout, 
   updateWorkout
 } = require('../controllers/workoutController')
+const requireAuth = require('../middleware/requireAuth')
 
 const router = express.Router()
+
+//authenticate before accessing routes
+router.use(requireAuth)
 
 // GET all workouts
 router.get('/', getWorkouts)
